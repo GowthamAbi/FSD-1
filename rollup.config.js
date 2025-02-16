602ac3e7385de969afea805df0df360711e4a21f
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-02-17 03:24:21 +0530

    update

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    input: 'src/index.js',
-    output: {
-      file: 'dist/bundle.js',
-      format: 'cjs',
-    },
-    external: ['@rollup/rollup-linux-x64-gnu'], // Exclude it from bundling
-  };
-  
